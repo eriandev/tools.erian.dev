00b08524efcb0732605fff6267fd6f804b420341
Erick Vargas <erianvc.dev@gmail.com>
2023-08-09 15:30:10 -0500

    feat(ccp): moves body styles to layout

---
+++ src/routes/calculadora-comisiones-paypal/+layout.svelte
@@ -0,0 +1,14 @@
+<slot />
+
+<style>
+  :global(body) {
+    color: theme('textColor.gray.900');
+  }
+
+  @media (prefers-color-scheme: dark) {
+    :global(body) {
+      color: theme('colors.white');
+      background: theme('colors.gray.900');
+    }
+  }
+</style>
