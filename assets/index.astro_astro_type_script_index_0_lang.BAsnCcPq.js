87aa972f5f6b072460cd26b4c672d04688ac0cb0
eriandev <4275190+eriandev@users.noreply.github.com>
2025-10-22 04:05:28 +0000

    Deploying to gh-pages from @ eriandev/tools.erian.dev@90e0ef3b74d825059249e979bf2ed77582c1392d 🚀

---
+++ assets/index.astro_astro_type_script_index_0_lang.BAsnCcPq.js
@@ -0,0 +1,1 @@
+import{u as o}from"./store.Bx_56UUZ.js";import"./runtime.CgkB6cgw.js";import"./dom.B6Sq5LsU.js";o().initBoardInfo();
--- assets/index.astro_astro_type_script_index_0_lang.DKZ0ii12.js
+++
@@ -1,1 +0,0 @@
-import{u as o}from"./store.CPM3PwoG.js";import"./runtime.B-cKuWPi.js";import"./dom.B6Sq5LsU.js";o().initBoardInfo();
