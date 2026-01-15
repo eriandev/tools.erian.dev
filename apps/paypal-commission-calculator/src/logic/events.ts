4d162af8fa6575351f8ea9538b3037b1f9dc184a
Erick Vargas <mail@erian.dev>
2026-01-14 20:58:37 -0500

    feat(pcc): copy values

--- apps/paypal-commission-calculator/src/logic/events.ts
+++ apps/paypal-commission-calculator/src/logic/events.ts
@@ -1,3 +1,4 @@
+import { toast } from 'wc-toast'
 import { getParsedValues, getRecieveValues, getSendValues } from './calculator-actions'
 import { $send, $amount, $receive, $sendCommission, $receiveCommission } from './declare-refs'
 
@@ -7,6 +8,31 @@ function isOnlyNumberOrDot({ key }: KeyboardEvent): boolean {
   const pattern = /^\d+(\.\d+)?$/
   return pattern.test(key)
 }
+
+document.addEventListener('click', (event: MouseEvent) => {
+  const target = event.target
+
+  if (!(target instanceof Element)) return
+
+  const svg = target.closest('svg[id^="copy-btn-"]')
+  if (svg === null) return
+
+  const targetId = svg.id.replace('copy-btn-', '')
+  const input = document.getElementById(targetId)
+
+  if (!(input instanceof HTMLInputElement)) return
+
+  void navigator.clipboard
+    .writeText(input.value)
+    .then(() => {
+      toast('Copied value!')
+    })
+    .catch((err: unknown) => {
+      if (err instanceof Error) {
+        console.error('Error coying:', err.message)
+      }
+    })
+})
 
 $amount?.addEventListener('input', ({ target }) => {
   const { value } = target as HTMLInputElement
